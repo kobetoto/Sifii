6a83ed8ad167f100354ee33eb57a2c76aae4cc19
tom dav <kobe242tomy@icloud.com>
2023-05-19 20:47:59 +0200

    May the 4 be with you

--- app.js
+++ app.js
@@ -2,6 +2,35 @@
 const express = require("express");
 const app = express();
 
+//express-session (cookies)
+const session = require("express-session");
+
+// since we are going to USE this middleware in the app.js,
+// let's export it and have it receive a parameter
+module.exports = (app) => {
+  // <== app is just a placeholder here
+  // but will become a real "app" in the app.js
+  // when this file gets imported/required there
+
+  // required for the app when deployed to Heroku (in production)
+  app.set("trust proxy", 1);
+
+  // use session
+  app.use(
+    session({
+      secret: process.env.SESS_SECRET,
+      resave: true,
+      saveUninitialized: false,
+      cookie: {
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+        secure: process.env.NODE_ENV === "production",
+        httpOnly: true,
+        //maxAge: 6000000, // 60 * 1000 ms === 1 min
+      },
+    })
+  );
+};
+
 //connect to the database
 require("./db");
 
